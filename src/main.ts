a9c6122ab4d0f9270942a4ed83fd5b2b601f0c67
Paul <ibeabuchi@worklio.cz>
2025-09-18 11:35:29 +0100

    added basic styles variables

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import PrimeVue from "primevue/config";
+import { createApp } from "vue";
+import App from "./App.vue";
+import routes from "./routes/routes";
+import "./style.css";
+import "./theme/theme.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue);
+app.use(routes);
+app.mount("#app");
