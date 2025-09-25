e45ee1dd66efede937aca6ae460a7ac8b6191703
Paul <ibeabuchi@worklio.cz>
2025-09-25 09:07:10 +0100

    added countries data

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
+import router from "@/router/router";
+import Aura from "@primeuix/themes/aura";
 import PrimeVue from "primevue/config";
 import { createApp } from "vue";
 import App from "./App.vue";
-import routes from "./routes/routes";
 import "./style.css";
 import "./theme/theme.css";
 
 const app = createApp(App);
-app.use(PrimeVue);
-app.use(routes);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      cssLayer: {
+        name: "primevue",
+        order: "base, primevue",
+      },
+    },
+  },
+});
+app.use(router);
 app.mount("#app");
